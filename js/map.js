a90b37cff5fd851ea17b6a70096003b6ae576975
Tay Clarke <tavia1120@yahoo.com>
2015-08-30 13:58:57 -0400

    Add Google Map Image

--- js/map.js
+++ js/map.js
@@ -1,17 +1,21 @@
+var map;
 function initialize() {
+
+  var locations;
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 13,
+    center: new google.maps.LatLng(37.6616, -77.6267)
   };
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-}
+};
 
-function loadScript() {
+
+  function loadScript() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCbCXLj3v6Z41i97C54y9w_CLuAYN8Pz0A' +
       '&signed_in=true&callback=initialize';
   document.body.appendChild(script);
 }
